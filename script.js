0fab8d1219f7976371d7895161991e99d14a4f6e
MEGANATH K M <meganathmoorthy@gmail.com>
2026-09-21 11:16:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+// =========================================
+// MEGNATH K M - PORTFOLIO
+// JAVASCRIPT
+// =========================================
 
+// Mobile menu
+const menuBtn = document.getElementById("menu-btn");
+const navbar = document.getElementById("navbar");
+
+menuBtn.addEventListener("click", () => {
+    navbar.classList.toggle("active");
+
+    const icon = menuBtn.querySelector("i");
+
+    icon.classList.toggle("fa-bars");
+    icon.classList.toggle("fa-xmark");
+});
+
+// Close mobile menu after clicking a link
+document.querySelectorAll(".navbar a").forEach(link => {
+    link.addEventListener("click", () => {
+        navbar.classList.remove("active");
+
+        const icon = menuBtn.querySelector("i");
+
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars");
+    });
+});
+
+// Automatically update footer year
+document.getElementById("year").textContent = new Date().getFullYear();
